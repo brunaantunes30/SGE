3faa9083e0bff12bc307e192d197529fc9d37be9
brunaantunes30 <brupipoliantunes@gmail.com>
2025-02-14 11:07:29 -0300

    MVC Escola

---
+++ Migrations/20250214131220_Notas.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SGE.Migrations
+{
+    /// <inheritdoc />
+    public partial class Notas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "NotasAlunos",
+                table: "Notas",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "NotasAlunos",
+                table: "Notas",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
---
+++ Models/Notas.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SGE.Models
+{
+    [Table("Notas")]
+    public class Notas
+    {
+        [Column("Id")]
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [ForeignKey("MateriasId")]
+        [Display(Name = "Matéria")]
+        public int MateriasId { get; set; }
+        public Materias? Materias { get; set; }
+
+        [ForeignKey("AlunosId")]
+        [Display(Name = "Aluno")]
+        public int AlunosId { get; set; }
+        public Alunos? Alunos { get; set; }
+
+        [ForeignKey("EtapasId")]
+        [Display(Name = "Etapa")]
+        public int EtapasId { get; set; }
+        public Etapas? Etapas { get; set; }
+
+        [Column("NotasAlunos")]
+        [Display(Name = "Notas")]
+        public double NotasAlunos { get; set; }
+    }
+}
